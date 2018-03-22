bc75e16b41fbce7be2365b5a77eaf2ae1b82b299
Kazi Israt Tori <kazisratori@gmail.com>
2018-03-22 15:26:48 +0600

    Added

--- UVa/195.cpp
+++ UVa/195.cpp
@@ -1,43 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool comp(char a, char b){
+	if(tolower(a) == tolower(b)){
+		return a<b;
+	}
+	return tolower(a) < tolower(b);
+}
 
 int main()
 {
     int t,i,j;
-    string a,b,c,d;
+    string a;
+    map<string,int>mp;
     scanf("%d",&t);
     getchar();
     while(t--)
     {
         cin>>a;
-        b="";
-        c="";
-        for(i=0;i<a.size();i++)
-        {
-            if(a[i]>='A' && a[i]<='Z') b+=a[i];
-            else c+=a[i];
-        }
-        sort(b.begin(),b.end());
-        sort(c.begin(),c.end());
-        i=0;j=0,d="";
-        while(i<b.size() && j<c.size())
-        {
-            if(tolower(b[i])<=c[j]){
-                d+=b[i];
-                i++;
-            }
-            else{
-                d+=c[j];
-                j++;
+        sort(a.begin(),a.end(),comp);
+        cout<<a<<endl;
+        mp[a]=1;
+        while(next_permutation(a.begin(),a.end(),comp)){
+            if(!mp[a]){
+                cout<<a<<endl;
+                mp[a]=1;
             }
         }
-
-        if(j==c.size()) for(;i<b.size();i++) d+=b[i];
-        if(i==b.size()) for(;j<c.size();j++) d+=c[j];
-        cout<<d<<endl;
-        while(next_permutation(d.begin(),d.end())){
-              cout<<d<<endl;
-        }
-
     }
 }
